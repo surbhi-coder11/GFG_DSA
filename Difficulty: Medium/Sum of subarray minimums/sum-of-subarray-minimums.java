963d6eeb71f0e7a6ea741f712cd022e0ebe6f417
Surbhi Gupta <bablooasr73@gmail.com>
2026-03-11 10:45:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of subarray minimums/sum-of-subarray-minimums.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int sumSubMins(int[] arr) {
+        // code here
+        int n = arr.length;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        // Previous Smaller Element
+        for(int i = 0; i < n; i++){
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]){
+                st.pop();
+            }
+            left[i] = st.isEmpty() ? i + 1 : i - st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        // Next Smaller or Equal Element
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+                st.pop();
+            }
+            right[i] = st.isEmpty() ? n - i : st.peek() - i;
+            st.push(i);
+        }
+
+        long ans = 0;
+
+        for(int i = 0; i < n; i++){
+            ans += (long)arr[i] * left[i] * right[i];
+        }
+
+        return (int)ans;
+
+    }
+}
