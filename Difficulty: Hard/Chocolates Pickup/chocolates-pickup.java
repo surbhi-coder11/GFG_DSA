3f3e9e0a8141b98b047f0d2fb9545645264f819d
Surbhi Gupta <bablooasr73@gmail.com>
2026-03-27 11:28:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int maxChocolate(int grid[][]) {
+        // code here
+        int n = grid.length;
+        int m = grid[0].length;
+           int[][][] dp = new int[n][m][m];
+
+        // Base case (last row)
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                if (j1 == j2)
+                    dp[n - 1][j1][j2] = grid[n - 1][j1];
+                else
+                    dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+            }
+        }
+
+        // Fill DP from bottom to top
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j1 = 0; j1 < m; j1++) {
+                for (int j2 = 0; j2 < m; j2++) {
+
+                    int max = 0;
+
+                    for (int d1 = -1; d1 <= 1; d1++) {
+                        for (int d2 = -1; d2 <= 1; d2++) {
+
+                            int nj1 = j1 + d1;
+                            int nj2 = j2 + d2;
+
+                            if (nj1 >= 0 && nj1 < m && nj2 >= 0 && nj2 < m) {
+
+                                int val;
+                                if (j1 == j2)
+                                    val = grid[i][j1];
+                                else
+                                    val = grid[i][j1] + grid[i][j2];
+
+                                val += dp[i + 1][nj1][nj2];
+
+                                max = Math.max(max, val);
+                            }
+                        }
+                    }
+
+                    dp[i][j1][j2] = max;
+                }
+            }
+        }
+
+        return dp[0][0][m - 1];
+    }
+}
