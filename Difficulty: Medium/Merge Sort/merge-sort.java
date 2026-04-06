94d97d216f29f43c06b0293902d750ece4ec80f9
Surbhi Gupta <bablooasr73@gmail.com>
2026-04-06 18:30:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -1,49 +1,42 @@
 class Solution {
 
     void mergeSort(int arr[], int l, int r) {
-        // code here
-        if(l<r){
-            int mid = (l+r)/2;
-            mergeSort(arr,l,mid);
-            mergeSort(arr,mid+1,r);
-            merge(arr,l , mid,r);
+        if(l>=r){
+            return;
         }
+        // code here
+        int m = l+(r-l)/2;
+        mergeSort(arr,l,m);
+        mergeSort(arr,m+1,r);
+        merge(arr,l,m,r);
     }
-    void merge(int[] arr , int l , int m , int r){
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        int L[] = new int[n1];
-        int R[] = new int[n2];
+    
+    void merge(int[] arr, int l , int m , int r){
+        int[] temp = new int[r-l+1];
+        int i=l;
+        int j=m+1;
+        int k=0;
         
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
-        int i = 0, j = 0;
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
+        while(i<=m && j<=r){
+            if(arr[i]<arr[j]){
+                temp[k] = arr[i];
                 i++;
-            }
-            else {
-                arr[k] = R[j];
+            }else{
+                temp[k] = arr[j];
                 j++;
             }
             k++;
         }
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+        
+        while(i<=m){
+           temp[k++] = arr[i++]; 
         }
-
-        // Copy remaining elements of R[] if any
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+        while(j<=r){
+            temp[k++] = arr[j++];
+        }
+        
+        for(k=0,i=l;k<temp.length;i++,k++){
+            arr[i] = temp[k];
         }
     }
 }
