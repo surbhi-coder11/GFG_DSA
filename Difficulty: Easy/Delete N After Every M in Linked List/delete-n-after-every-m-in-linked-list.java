969710cc9a99180643049b3a6162b1f22085a9f1
Surbhi Gupta <bablooasr73@gmail.com>
2026-09-16 16:19:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete N After Every M in Linked List/delete-n-after-every-m-in-linked-list.java
+++ Difficulty: Easy/Delete N After Every M in Linked List/delete-n-after-every-m-in-linked-list.java
@@ -1,29 +1,35 @@
+/* Structure of Linked List Node
+class Node
+{
+  int data;
+  Node next;
+  Node(int data)
+  {
+      this.data = data;
+      this.next = null;
+  }
+}*/
+
 class Solution {
     static void linkDelete(Node head, int n, int m) {
-        Node curr = head;
-
-        while (curr != null) {
-            // 1. Skip m - 1 nodes to land on the m-th node
-            for (int count = 1; count < m && curr != null; count++) {
-                curr = curr.next;
-            }
-
-            // If we reached the end while skipping, stop
-            if (curr == null) {
-                return;
-            }
-
-            // 2. Start from the node after the m-th node and skip n nodes
-            Node temp = curr.next;
-            for (int count = 1; count <= n && temp != null; count++) {
-                temp = temp.next;
-            }
-
-            // 3. Link the m-th node to the node after the n deleted nodes
-            curr.next = temp;
-
-            // 4. Move curr to resume the process
-            curr = temp;
+        // code here
+         Node curr = head;
+        while(curr!=null){
+    
+        for(int count =1; count<m && curr!=null ; count++){
+            curr = curr.next;
+        }
+        if(curr == null){
+            return;
+        }
+        
+        Node temp = curr.next;
+        
+        for(int i=1;i<=n && temp!=null;i++){
+            temp= temp.next;
+        }
+        curr.next = temp;
+        curr= temp;
         }
     }
 }
